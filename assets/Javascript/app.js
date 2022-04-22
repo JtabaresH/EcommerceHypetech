0c33fd4127fab11335c943827029df4efc1ff576
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-04-22 00:56:02 -0500

    Añadir arreglo del carrito}

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,5 +1,6 @@
 const contenedorProductos = document.getElementById('container')
 
+let carrito = []
 
 stockProductos.forEach((producto) => {
     const div = document.createElement('div')
@@ -8,8 +9,20 @@ stockProductos.forEach((producto) => {
     <img src=${producto.img} alt="">
     <h4>${producto.nombre}</h4>
     <p>Stock: ${producto.cantidad}</p>
-    <p>Precio: $ ${producto.precio}</p>
+    <p>Precio: ${producto.precio} USD</p>
     <button id="agregar${producto.id}" class="boton-agregar">Añadir al carrito</button>
     `
     contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+    
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id)
+    })
 })
+
+const agregarAlCarrito = (prodId) => {
+    const item = stockProductos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    console.log(carrito)
+}
