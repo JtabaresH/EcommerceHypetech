e9bc5638d42ebcf8e5a459895d6f4bc40cc02beb
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-04-23 00:17:56 -0500

    Avance carrito

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,6 +1,20 @@
 const contenedorProductos = document.getElementById('container')
-
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const precioTotal = document.getElementById('precioTotal')
 let carrito = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
 
 stockProductos.forEach((producto) => {
     const div = document.createElement('div')
@@ -22,7 +36,44 @@ stockProductos.forEach((producto) => {
 })
 
 const agregarAlCarrito = (prodId) => {
-    const item = stockProductos.find((prod) => prod.id === prodId)
-    carrito.push(item)
-    console.log(carrito)
+    const existe = carrito.some(prod => prod.id === prodId)
+    if (existe) {
+        const producto = carrito.map(prod => {
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else {
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        carrito.push(item)
+    }
+    actualizarCarrito()
+}
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1) 
+
+    actualizarCarrito()
+}
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((prod) => {
+       const div = document.createElement('div')
+       div.className = ('productoEnCarrito')
+       div.innerHTML = `
+       <img
+       <p>${prod.nombre}</p>
+       <p>Precio: ${prod.precio}</p>
+       <p>Cantidad: <span id="cantidad">${prod.cantidad}</p>
+       <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></button>
+       `
+       contenedorCarrito.appendChild(div)
+       localStorage.setItem('carrito', JSON.stringify(carrito))
+    })
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
 }
